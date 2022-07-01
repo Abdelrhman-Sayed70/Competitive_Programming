6d02d6f3210a477427517d449951759460b9c115
Abdo sayed <abdulrhmansayed70@gmail.com>
2022-07-02 00:10:30 +0200

    update

---
+++ Codes/Math/isperfectsquare.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+bool isperfectsqaur(ll n) {
+	ll tmp = sqrt(n);
+	return((tmp * tmp) == n);
+}
+int main() {
+	int t = 10; 
+	while (t--) {
+		ll n; 
+		cin >> n; 
+		cout << isperfectsqaur(n) << "\n";
+	}
+    
+}
