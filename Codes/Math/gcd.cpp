22be660145f8b48182c9b1f38f5ee321e3f9d1f6
Abdo sayed <abdulrhmansayed70@gmail.com>
2022-07-01 14:10:15 +0200

    update

---
+++ Codes/Math/gcd.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ll gcd(ll a,  ll b) {
+    // recursive 
+    // gcd(a,b) =gcd(b,a%b)
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+ll gcd(ll a, ll b) {
+    // iterative 
+    // gcd(a,b) =gcd(b,a%b)
+    while (b != 0) {
+        ll a2 = a;
+        a = b;
+        b = a2 % b;
+    }
+    return a;
+}
+int main() {
+    // GCD
+}
