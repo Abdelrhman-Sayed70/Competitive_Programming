d95655abca732fbd3e1c334d77c9a1f008ecc11e
Abdo sayed <abdulrhmansayed70@gmail.com>
2022-07-02 13:48:30 +0200

    update

---
+++ Codes/STLs/memset.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main() {
+	bool arr[10];
+	memset(arr, 2, sizeof(arr));
+	for (int i = 0; i < sizeof(arr); i++) {
+		cout << arr[i] << " ";
+	}
+}
