74a9631ea5d01d609d3fe8153b4875c4a460694b
Abdo sayed <abdulrhmansayed70@gmail.com>
2022-07-12 05:43:04 +0200

    update

--- Codes/Math & number theory/from a to b/sum for even numbers form a to b .cpp
+++ Codes/Math & number theory/from a to b/sum for even numbers form a to b .cpp
@@ -2,16 +2,18 @@
 using namespace std;
 #define ll long long
 
-ll sumall(ll a, ll n) {
+ll sumeven(ll a, ll n) {
     // Formula : sum = (first number + last number) * (number of numbers) / 2
     // in main make sure that n >= a
-    ll numOfnums = n - a + 1;
+    if (a % 2) a++;
+    if (n % 2) n--;
+    ll numOfnums = ((n - a) / 2) + 1;
     ll sum = (a + n) * numOfnums / 2;
     return sum;
 }
 int main() {
     int a, b;
     cin >> a >> b;
-    if (b >= a) { cout << sumall(a, b); }
-    else  cout << sumall(1, a);
+    if (b >= a) { cout << sumeven(a, b); }
+    else  cout << sumeven(1, a);
 }
--- Codes/Math & number theory/from a to b/sum for odd numbers form a to b  .cpp
+++ Codes/Math & number theory/from a to b/sum for odd numbers form a to b  .cpp
@@ -2,16 +2,18 @@
 using namespace std;
 #define ll long long
 
-ll sumall(ll a, ll n) {
+ll sumodd(ll a, ll n) {
     // Formula : sum = (first number + last number) * (number of numbers) / 2
     // in main make sure that n >= a
-    ll numOfnums = n - a + 1;
+    if (a % 2 == 0) a++;
+    if (n % 2 == 0) n--;
+    ll numOfnums = ((n - a) / 2) + 1;
     ll sum = (a + n) * numOfnums / 2;
     return sum;
 }
 int main() {
     int a, b;
     cin >> a >> b;
-    if (b >= a) { cout << sumall(a, b); }
-    else  cout << sumall(1, a);
+    if (b >= a) { cout << sumodd(a, b); }
+    else  cout << sumodd(1, a);
 }
