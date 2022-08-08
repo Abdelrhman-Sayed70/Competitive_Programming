f5cf641a0397bba0fd68e20f3eda2e94d21efc1f
Abdo sayed <abdulrhmansayed70@gmail.com>
2022-08-09 00:24:54 +0200

    update

--- Math & Number Theory/GCD & LCM/gcd.cpp
+++ Math & Number Theory/GCD & LCM/gcd.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+﻿#include<bits/stdc++.h>
 using namespace std;
 #define ll long long
 ll gcd(ll a,  ll b) {
@@ -21,14 +21,13 @@ ll gcd(ll a, ll b) {
 }
 int main() {
   /*
-
+    Def :
     GCD(Greatest common devisor) >> The highest number that divides exactly into two or more numbers.
-    the answer will be less than or equal the greatest number 
+    the answer will be less than or equal the least number 
 
-    gcd(0,any number) = number
-    gcd(1 ,any number) = 1  
-      
+    prop : 
+    -gcd (a, 0) = |a|, 
+    -gcd(1 ,any number) = 1  
+    -if a divides the product b ⋅ c, and gcd (a, b) = d, then a / d divides c.
   */
-
- 
 }
