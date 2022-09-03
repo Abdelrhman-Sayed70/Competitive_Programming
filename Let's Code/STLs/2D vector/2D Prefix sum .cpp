156358fd64cfaa1b0e43eef749813462a0bd8ff5
Abdo sayed <abdulrhmansayed70@gmail.com>
2022-09-03 20:25:06 +0200

    update

--- Let's Code/STLs/2D vector/2D Prefix sum .cpp
+++ Let's Code/STLs/2D vector/2D Prefix sum .cpp
@@ -3,35 +3,35 @@ using namespace std;
 #define ll long long 
 
 int main() { 
-    // 2D prefix sum ; 
+    // range 2D subarray sum 
     int n, m; 
     cin >> n >> m; 
-    vector<vector<int>>v(n, vector<int>(m)),prefix(n,vector<int>(m)); 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    vector<vector<int>>v(n+1, vector<int>(m+1)),prefix(n+1,vector<int>(m+1)); 
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
             cin >> v[i][j];
         }
     }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
             // 4 cases  
-            if (i == 0 and j == 0) {
+            if (i == 1 and j == 1) {
                 prefix[i][j] = v[i][j];
             }
-            else if (i == 0 and j > 0) {
+            else if (i == 1 and j > 1) {
                 prefix[i][j] = prefix[i][j - 1] + v[i][j];
             }
-            else if (i > 0 and j == 0) {
+            else if (i > 1 and j == 1) {
                 prefix[i][j] = prefix[i - 1][j] + v[i][j];
             }
-            else if (i > 0 and j > 0) {
+            else if (i > 1 and j > 1) {
                 prefix[i][j] = prefix[i - 1][j] + prefix[i][j - 1] - prefix[i - 1][j - 1] + v[i][j];
             }
         }
     }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << prefix[i][j] << " ";
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= m; j++) {
+            cout << prefix[i][j] << " "; 
         }
         cout << "\n";
     }
