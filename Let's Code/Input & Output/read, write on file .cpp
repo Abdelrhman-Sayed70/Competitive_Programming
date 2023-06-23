fd8852a5c24c11a204bb4749c51fb9b9d54f14e1
Abdelrhman Sayed <abdulrhmansayed70@gmail.com>
2023-06-23 03:16:03 +0300

    update

--- Let's Code/Input & Output/read, write on file .cpp
+++
@@ -1,7 +0,0 @@
-void fileInputOutput() {
-#pragma warning(disable : 4996).
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-}
--- Let's Code/Input & Output/standred input , standred output .cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long
-
-int main() {
-	int n; 
-	cin >> n; 
-	cout << n;
-}
