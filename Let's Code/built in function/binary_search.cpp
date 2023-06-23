3a267bc73f6354b352799831cf3e88a605316c8f
Abdelrhman Sayed <abdulrhmansayed70@gmail.com>
2023-06-23 03:11:09 +0300

    update

--- Let's Code/built in function/binary_search.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long
-
-int main() {
-	vector<int>v = { 1,1,3 };
-	if (binary_search(v.begin(), v.end(), 3)) { cout << "YES"; }
-	else cout << "NO";
-}
--- Let's Code/built in function/is_sorted.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-#define ll long long
-
-int main() {
-	vector<int>v = { 1,1,3 };
-	cout << is_sorted(v.begin(),v.end());
-}
