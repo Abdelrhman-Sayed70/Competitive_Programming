1ec9d8ed52e25ae450fb8d4cc2965d079d1428d4
Abdelrhman Sayed <abdulrhmansayed70@gmail.com>
2023-06-24 18:04:07 +0300

    update

--- Let's Code/Searching/Binary Search/problems/count number of pairs using lower upper bound .cpp
+++
@@ -1,6 +0,0 @@
-tmp = power_of_tow - v[i];
-auto low = lower_bound(v.begin() + i + 1, v.end(), tmp);
-auto up = upper_bound(v.begin() + i + 1, v.end(), tmp);
-ll index1 = low - v.begin();
-ll index2 = up - v.begin();
-cnt += index2 - index1;
